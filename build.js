2c96fb19d23a2a7377ef0e5ef77905b2b6873e87
Irek Mirgaleev <irek.mirgaleev@acquia.com>
2016-06-04 17:50:45 -0400

    Add the build.js script that builds a ZIP file used to upload to Chrome Developer Dashboard

---
+++ build.js
@@ -0,0 +1,37 @@
+var mkdirp = require('mkdirp');
+var ncp = require('ncp').ncp;
+var rimraf = require('rimraf');
+var zipFolder = require('zip-folder');
+
+var async = require('async');
+
+async.series([
+	function (callback) {
+		rimraf('extension-dist.zip', [], callback);	
+	},
+	function (callback) {
+		rimraf('dist', [], callback);
+	},
+	function (callback) {
+		mkdirp('dist', callback);
+	},
+	function (callback) {
+		ncp('images', 'dist/images', callback);
+	},
+	function (callback) {
+		ncp('lib', 'dist/lib', callback);
+	},
+	function (callback) {
+		ncp('manifest.json', 'dist/manifest.json', callback);
+	},
+	function (callback) {
+		ncp('src', 'dist/src', callback);
+	},
+	function (callback) {
+		zipFolder('dist', 'extension-dist.zip', callback);
+	},
+	function (callback) {
+		rimraf('dist', [], callback);
+	}
+]);
+
