702dfbac1be07b57e271c6f9d942816042059d03
Irek Mirgaleev <irek.mirgaleev@acquia.com>
2019-03-25 09:24:42 -0400

    fix latest github update

--- src/main.js
+++ src/main.js
@@ -29,7 +29,7 @@ Main.prototype.generateApp = function () {
 
   var files = [];
   $.each($('.file'), function (index, item) {
-    var file = $(item).find('.file-header').data('path');
+    var file = $(item).find('.file-header[data-path]').data('path');
     if (file) {
       files[index] = file;
     }
